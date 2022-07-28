50290e10aa930ac3c5bcdc667ca0b29d72135ce8
Jay <chriese0112@gmail.com>
2022-07-28 17:48:23 +0800

    Add advanced assignments

--- part-2/assignment-advanced4.js
+++ part-2/assignment-advanced4.js
@@ -8,11 +8,11 @@ function practice_4(input) {
   // your code here
 }
 
-const input3 = [
+const input4 = [
   { key: "a", value: 3 },
   { key: "b", value: 1 },
   { key: "c", value: 2 },
   { key: "a", value: 3 },
   { key: "c", value: 5 },
 ];
-console.log(practice_3(input4)); // expected output: {'a': 6, 'b': 1, 'c': 7}
+console.log(practice_4(input4)); // expected output: {'a': 6, 'b': 1, 'c': 7}
