816843a79e88d56471041953753c806144ab0a82
Jay <chriese0112@gmail.com>
2022-08-01 20:53:19 +0800

    Add advanced assignment

--- part-3/advanced/advanced.js
+++ part-3/advanced/advanced.js
@@ -6,7 +6,7 @@ function getFetch(url) {
   return fetch(url).then((response) => response.json());
 }
 
-function rander(data) {
+function render(data) {
   let li = "";
   let innerLi = "";
   for (let i = 0; i < data.length; i++) {
@@ -17,39 +17,34 @@ function rander(data) {
     } else {
       innerLi = "";
     }
-
     li += `
-  <li>
-    <header>
-      <h2>${data[i].name}</h2>
-      <div class="visibility">${data[i].visibility}</div>
-    </header>
-    <article>
-      <p>
-        ${data[i].description}
-      </p>
-    </article>
-    <footer>
-      <ul class="inner-ul">
-      ${innerLi}
-      </ul>
-    </footer>
-  </li>`;
-
-    // console.log("topics", data[i].topics);
+    <li>
+      <header>
+        <h2>${data[i].name}</h2>
+        <div class="visibility">${data[i].visibility}</div>
+      </header>
+      <article>
+        <p>
+          ${data[i].description}
+        </p>
+      </article>
+      <footer>
+        <ul class="inner-ul">
+        ${innerLi}
+        </ul>
+      </footer>
+    </li>`;
   }
   displayList.innerHTML = li;
 }
 
 moreBtn.addEventListener("click", () => {
-  console.log("click");
   num += 5;
-  console.log(num);
   getFetch(
     `https://api.github.com/orgs/facebook/repos?per_page=${num}&page=1`
-  ).then((data) => rander(data));
+  ).then((data) => render(data));
 });
 
 getFetch(`https://api.github.com/orgs/facebook/repos?per_page=5&page=1`).then(
-  (data) => rander(data)
+  (data) => render(data)
 );
