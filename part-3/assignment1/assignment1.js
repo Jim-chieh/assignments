e0819becdb80aca35a0f17d6bf86e296018f2c5f
Jay <chriese0112@gmail.com>
2022-08-01 13:53:52 +0800

    Fixed assignment1 files

--- part-3/assignment1/assignment1.js
+++ part-3/assignment1/assignment1.js
@@ -1,8 +1,11 @@
+//修改message
 const header = document.querySelector("header");
 const h1 = document.querySelectorAll("h1");
+//操控menu
 const p = document.querySelector(".nav-p");
 const div = document.querySelector("body div");
 const exit = document.querySelector(".menu-exit");
+//增加/隱藏contentBox
 const showMore = document.querySelector(".section-content button");
 const content = document.querySelector("#content-ul");
 
