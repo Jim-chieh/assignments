bb2706e6d9eb618f0540057471ba1cc9d888cfcb
Jay <chriese0112@gmail.com>
2022-08-01 13:32:38 +0800

    Add assignment2 files

---
+++ part-3/assignment2/assignment2.js
@@ -0,0 +1,27 @@
+const ul = document.querySelector("ul");
+
+function ajax(url, callback) {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => callback(data));
+}
+
+function render(data) {
+  console.log(data);
+  let li = "";
+  for (let i = 0; i < data.length; i++) {
+    li += `<li>
+            <h1>${data[i].name}</h1>
+            <h2>$: ${data[i].price}</h2>
+            <p>${data[i].description}</p>
+            </li>`;
+  }
+  ul.innerHTML = li;
+}
+
+ajax(
+  "https://appworks-school.github.io/Remote-Aassigiment-Data/products",
+  function (data) {
+    render(data);
+  }
+);
