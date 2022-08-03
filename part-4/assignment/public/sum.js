c99e580427e28b42e39b4e8c98d2d50b40091d6d
Jay <chriese0112@gmail.com>
2022-08-03 15:09:38 +0800

    Commit all changes

--- part-4/assignment/public/sum.js
+++ part-4/assignment/public/sum.js
@@ -4,8 +4,7 @@ const content = document.querySelector("p");
 submitButton.addEventListener("click", fetchData);
 function fetchData() {
   let value = input.value;
-  const url = "http://localhost:3000/getData/?number=";
-  fetch(url + value)
+  fetch("http://localhost:3000/getData/?number=" + value)
     .then((response) => response.json())
     .then((data) => (content.textContent = `Total number is :${data}`));
   input.value = "";
